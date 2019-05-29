e14a63de923d195a6abafe2e19dfe3f4592202c4
Karolina Modlinska <karolina.rzeznik@gmail.com>
2019-05-29 13:48:39 +0200

    Menu and section home

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,7 @@ gulp.task("sass", function () {
         .pipe(sourcemaps.write())
         .pipe(gulp.dest("css"))
 });
+
 gulp.task("default", function () {
     gulp.watch("./scss/**/*.scss", gulp.series("sass"));
     gulp.watch("./js/**/*.js", gulp.series("jshint"));
